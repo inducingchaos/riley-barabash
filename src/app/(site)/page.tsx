a6c38bd60b54f8387f2f8415eaded01810c7f5c3
Riley Barabash <admin@rileybarabash.com>
2024-09-11 01:31:27 -0600

    feat: test PFP on landing

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -12,6 +12,7 @@
  * #tsx
  */
 
+import Image from "next/image"
 import Link from "next/link"
 import { Button } from "~/components/ui/primitives/inputs"
 import { H1, InlineCode, Muted } from "~/components/ui/primitives/typography"
@@ -30,7 +31,10 @@ export default async function Landing(): Promise<JSX.Element> {
                     <section className="flex min-h-screen flex-col items-center justify-center">
                         {/* Content. */}
 
-                        <div className="flex flex-col items-center justify-center gap-4">
+                        <div className="flex flex-col items-center justify-center gap-6">
+                            <div className="relative h-24 w-24">
+                                <Image src="/brand/pfp.jpg" alt="Profile Picture" fill className="rounded-xl object-cover" />
+                            </div>
                             <H1>{"My name is Riley."}</H1>
                             <InlineCode>{"X/IG: @inducingchaos"}</InlineCode>
                             <Muted>{"This won't last long. Take a look around."}</Muted>
