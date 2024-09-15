280fd610b6cfb40359c5804ba2866d2a97e461b7
Riley Barabash <admin@rileybarabash.com>
2024-09-15 09:27:45 -0600

    fix: i'm done being mad

--- src/app/(subdomains)/kyzn/middleware.ts
+++ src/app/(subdomains)/kyzn/middleware.ts
@@ -3,3 +3,5 @@ import { type NextRequest, type NextResponse } from "next/server"
 export async function middleware(_request: NextRequest): Promise<NextResponse | undefined> {
     return undefined
 }
+
+//
