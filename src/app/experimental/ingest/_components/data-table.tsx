295713f76a6186b8ab93bbea098309e2ff06efe0
Riley Barabash <admin@rileybarabash.com>
2024-09-22 21:48:41 -0600

    fix: cmd-a cmd-slash

--- src/app/experimental/ingest/_components/data-table.tsx
+++ src/app/experimental/ingest/_components/data-table.tsx
@@ -1,164 +1,164 @@
-"use client"
+// "use client"
 
-import {
-    type ColumnDef,
-    type ColumnFiltersState,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    type SortingState,
-    useReactTable,
-    type VisibilityState
-} from "@tanstack/react-table"
-import { useState, useMemo, forwardRef } from "react"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/primitives/display"
-import {
-    Button,
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-    Input
-} from "~/components/ui/primitives/inputs"
-import { getColumns } from "./columns"
-import { DataTablePagination } from "./table/pagination"
-import { DataTableViewOptions } from "./table/column-toggle"
+// import {
+//     // type ColumnDef,
+//     type ColumnFiltersState,
+//     flexRender,
+//     getCoreRowModel,
+//     getFilteredRowModel,
+//     getPaginationRowModel,
+//     getSortedRowModel,
+//     type SortingState,
+//     useReactTable,
+//     type VisibilityState
+// } from "@tanstack/react-table"
+// import { useState, useMemo } from "react"
+// import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/primitives/display"
+// import {
+//     // Button,
+//     // DropdownMenu,
+//     // DropdownMenuCheckboxItem,
+//     // DropdownMenuContent,
+//     // DropdownMenuTrigger,
+//     Input
+// } from "~/components/ui/primitives/inputs"
+// import { getColumns } from "./columns"
+// import { DataTablePagination } from "./table/pagination"
+// import { DataTableViewOptions } from "./table/column-toggle"
 
-// interface DataTableProps<TData, TValue> {
-//     columns: ColumnDef<TData, TValue>[]
+// // interface DataTableProps<TData, TValue> {
+// //     columns: ColumnDef<TData, TValue>[]
+// //     data: TData[]
+// // }
+
+// interface DataTableProps<TData> {
 //     data: TData[]
 // }
+// /**
+//  * Restore the original func sig
+//  */
+// export function DataTable<TData extends Record<string, any>>({ data }: DataTableProps<TData>) {
+//     const columns = useMemo(() => getColumns(data), [data])
 
-interface DataTableProps<TData> {
-    data: TData[]
-}
-/**
- * Restore the original func sig
- */
-export function DataTable<TData extends Record<string, any>>({ data }: DataTableProps<TData>) {
-    const columns = useMemo(() => getColumns(data), [data])
+//     const [sorting, setSorting] = useState<SortingState>([])
+//     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+//     const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+//     const [rowSelection, setRowSelection] = useState({})
 
-    const [sorting, setSorting] = useState<SortingState>([])
-    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
-    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = useState({})
+//     const table = useReactTable({
+//         data,
+//         columns,
+//         getCoreRowModel: getCoreRowModel(),
+//         getPaginationRowModel: getPaginationRowModel(),
+//         onSortingChange: setSorting,
+//         getSortedRowModel: getSortedRowModel(),
+//         onColumnFiltersChange: setColumnFilters,
+//         getFilteredRowModel: getFilteredRowModel(),
+//         onColumnVisibilityChange: setColumnVisibility,
+//         onRowSelectionChange: setRowSelection,
+//         state: {
+//             sorting,
+//             columnFilters,
+//             columnVisibility,
+//             rowSelection
+//         }
+//     })
 
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        onSortingChange: setSorting,
-        getSortedRowModel: getSortedRowModel(),
-        onColumnFiltersChange: setColumnFilters,
-        getFilteredRowModel: getFilteredRowModel(),
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        state: {
-            sorting,
-            columnFilters,
-            columnVisibility,
-            rowSelection
-        }
-    })
+//     return (
+//         <div>
+//             <div className="flex w-screen flex-col items-center justify-center p-8">
+//                 <div className="flex w-full items-center justify-between py-4">
+//                     <div className="h-1 w-8 border-2"></div>
+//                     <Input
+//                         placeholder="Filter content..."
+//                         value={(table.getColumn("content")?.getFilterValue() as string) ?? ""}
+//                         onChange={event => table.getColumn("content")?.setFilterValue(event.target.value)}
+//                         className="max-w-sm rounded-none border-2 shadow-none"
+//                     />
+//                     <DataTableViewOptions table={table} />
 
-    return (
-        <div>
-            <div className="flex w-screen flex-col items-center justify-center p-8">
-                <div className="flex w-full items-center justify-between py-4">
-                    <div className="h-1 w-8 border-2"></div>
-                    <Input
-                        placeholder="Filter content..."
-                        value={(table.getColumn("content")?.getFilterValue() as string) ?? ""}
-                        onChange={event => table.getColumn("content")?.setFilterValue(event.target.value)}
-                        className="max-w-sm rounded-none border-2 shadow-none"
-                    />
-                    <DataTableViewOptions table={table} />
-
-                    {/* <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" className="ml-auto">
-                                Columns
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            {table
-                                .getAllColumns()
-                                .filter(column => column.getCanHide())
-                                .map(column => {
-                                    return (
-                                        <DropdownMenuCheckboxItem
-                                            key={column.id}
-                                            className="capitalize"
-                                            checked={column.getIsVisible()}
-                                            onCheckedChange={value => column.toggleVisibility(!!value)}
-                                        >
-                                            {column.id}
-                                        </DropdownMenuCheckboxItem>
-                                    )
-                                })}
-                        </DropdownMenuContent>
-                    </DropdownMenu> */}
-                </div>
-                <div className="w-full border-2">
-                    <Table>
-                        <TableHeader className="border-b-2">
-                            {table.getHeaderGroups().map(headerGroup => (
-                                <TableRow key={headerGroup.id}>
-                                    {headerGroup.headers.map(header => {
-                                        return (
-                                            <TableHead key={header.id}>
-                                                {header.isPlaceholder
-                                                    ? null
-                                                    : flexRender(header.column.columnDef.header, header.getContext())}
-                                            </TableHead>
-                                        )
-                                    })}
-                                </TableRow>
-                            ))}
-                        </TableHeader>
-                        <TableBody>
-                            {table.getRowModel().rows?.length ? (
-                                table.getRowModel().rows.map(row => (
-                                    <TableRow
-                                        className="border-b-2"
-                                        key={row.id}
-                                        data-state={row.getIsSelected() && "selected"}
-                                    >
-                                        {row.getVisibleCells().map(cell => (
-                                            <TableCell key={cell.id}>
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))
-                            ) : (
-                                <TableRow>
-                                    <TableCell colSpan={columns.length} className="h-24 text-center">
-                                        No results.
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </div>
-                <div className="w-full">
-                    <DataTablePagination table={table} />
-                </div>
-            </div>
-            {/* <div className="flex items-center justify-end space-x-2 py-4">
-                <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-                    Previous
-                </Button>
-                <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-                    Next
-                </Button>
-            </div>
-            <div className="flex-1 text-sm text-muted-foreground">
-                {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
-            </div> */}
-        </div>
-    )
-}
+//                     {/* <DropdownMenu>
+//                         <DropdownMenuTrigger asChild>
+//                             <Button variant="outline" className="ml-auto">
+//                                 Columns
+//                             </Button>
+//                         </DropdownMenuTrigger>
+//                         <DropdownMenuContent align="end">
+//                             {table
+//                                 .getAllColumns()
+//                                 .filter(column => column.getCanHide())
+//                                 .map(column => {
+//                                     return (
+//                                         <DropdownMenuCheckboxItem
+//                                             key={column.id}
+//                                             className="capitalize"
+//                                             checked={column.getIsVisible()}
+//                                             onCheckedChange={value => column.toggleVisibility(!!value)}
+//                                         >
+//                                             {column.id}
+//                                         </DropdownMenuCheckboxItem>
+//                                     )
+//                                 })}
+//                         </DropdownMenuContent>
+//                     </DropdownMenu> */}
+//                 </div>
+//                 <div className="w-full border-2">
+//                     <Table>
+//                         <TableHeader className="border-b-2">
+//                             {table.getHeaderGroups().map(headerGroup => (
+//                                 <TableRow key={headerGroup.id}>
+//                                     {headerGroup.headers.map(header => {
+//                                         return (
+//                                             <TableHead key={header.id}>
+//                                                 {header.isPlaceholder
+//                                                     ? null
+//                                                     : flexRender(header.column.columnDef.header, header.getContext())}
+//                                             </TableHead>
+//                                         )
+//                                     })}
+//                                 </TableRow>
+//                             ))}
+//                         </TableHeader>
+//                         <TableBody>
+//                             {table.getRowModel().rows?.length ? (
+//                                 table.getRowModel().rows.map(row => (
+//                                     <TableRow
+//                                         className="border-b-2"
+//                                         key={row.id}
+//                                         data-state={row.getIsSelected() && "selected"}
+//                                     >
+//                                         {row.getVisibleCells().map(cell => (
+//                                             <TableCell key={cell.id}>
+//                                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
+//                                             </TableCell>
+//                                         ))}
+//                                     </TableRow>
+//                                 ))
+//                             ) : (
+//                                 <TableRow>
+//                                     <TableCell colSpan={columns.length} className="h-24 text-center">
+//                                         No results.
+//                                     </TableCell>
+//                                 </TableRow>
+//                             )}
+//                         </TableBody>
+//                     </Table>
+//                 </div>
+//                 <div className="w-full">
+//                     <DataTablePagination table={table} />
+//                 </div>
+//             </div>
+//             {/* <div className="flex items-center justify-end space-x-2 py-4">
+//                 <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+//                     Previous
+//                 </Button>
+//                 <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+//                     Next
+//                 </Button>
+//             </div>
+//             <div className="flex-1 text-sm text-muted-foreground">
+//                 {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
+//             </div> */}
+//         </div>
+//     )
+// }
