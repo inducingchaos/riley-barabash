25403a1e13578a2a78ecdfbb1bbbddc41f4dca66
Riley Barabash <admin@rileybarabash.com>
2024-10-14 01:10:23 -0600

    feat: dev mode rewrite

--- src/app/api/comms/sms/route.ts
+++ src/app/api/comms/sms/route.ts
@@ -3,40 +3,21 @@
  */
 
 import { NextResponse, type NextRequest } from "next/server"
-import { application } from "~/config"
 import { composeResponse } from "~/lib/comms/sms/compose-response"
 import { decodeParams, type DecodeParamsResult } from "~/lib/comms/sms/decode-params"
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-    //  Decode the request
-
-    // Check if the request is made to the base URL
-    // const headersList = headers()
-    // const host = headersList.get("host")
-    // const baseUrl = `${req.nextUrl.protocol}//${host}`
-
-    if (req.url?.startsWith(application.routing.urls.base ?? "")) {
-        // Forward the request to a placeholder URL
-        const url = `${process.env.NEXT_PUBLIC_DEVTUNNEL_URL}/api/comms/sms`
-        const forwardedResponse = await fetch(url, {
-            method: req.method,
-            headers: req.headers,
-            body: req.body
-        })
-
-        return NextResponse.json(await forwardedResponse.json(), {
-            status: forwardedResponse.status,
-            headers: forwardedResponse.headers
-        })
-    }
-
+    const requestUrl = new URL(req.url)
     const messageParams: DecodeParamsResult = await decodeParams({ req })
 
-    //  Process the message params and produce a response
+    if (
+        requestUrl.hostname.endsWith("rileybarabash.com") &&
+        messageParams.Body.toLowerCase().startsWith("/dev") &&
+        messageParams.From.endsWith("0221")
+    )
+        return NextResponse.rewrite(new URL(requestUrl.pathname, "https://39nkpp9k-221.usw2.devtunnels.ms"))
 
     // const response: string | null = await coordinateResponse(messageParams)
 
-    //  Generate a response and send it back to Twilio
-
-    return composeResponse({ content: `Hi ${req.url}! ${messageParams.Body}` })
+    return composeResponse({ content: `${requestUrl.port ? "[dev] " : ""}Received: ${messageParams.Body}` })
 }
